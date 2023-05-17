fec2a5d26a11199da3ecb3e031a2ce68de5baf84
Diyaa Alhaik <diyaahaik77@gmail.com>
2023-05-17 14:35:55 +0300

    foodera

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,29 @@ import './Home.css'
 import Header from './Header'
 import pridimg from '../../img/1.png'
 import ingredients from '../../img/2.png'
+import Data from '../../Data'
+import Carousel from 'react-bootstrap/Carousel';
+import user1 from '../../img/1 (1).jpg';
+import user2 from '../../img/2.jpg'
+
  function Home() {
+
+
+
+  const blogitme =Data.map(itme =>{
+    return(
+      <div className='col-md-4'>
+        <div className='box'>
+        <img key={itme.id} src={itme.img}/>
+        <h5 key={itme.id}>{itme.title}</h5>
+      <span key={itme.id}>{itme.time}</span>
+        <h6 key={itme.id}>{itme.price}</h6>
+        <button href='#'>order now</button>
+        </div>
+      
+      </div>  
+    )
+  });
   return (
     <div>
     <Header/>
@@ -88,6 +110,59 @@ We make everything by hand with the best possible ingredients.
         </div>
       </div>
     </div>
+  </section>
+  <section className='blogs'>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-lg-12 col-md-12'>
+<h2>Explore Our Foods</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat tellus. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+        </div>
+      </div>
+      <div className='row'>
+        {blogitme}
+      </div>
+    </div>
+  </section>
+  <section className='slider'>
+
+  <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100"src={user1} alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={user2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={user2}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
   </section>
     </div>
   )
